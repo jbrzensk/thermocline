ec42b0438a027406903ee67150bc42d2570b29ae
Jared Brzenski <jbrzenski@sdsu.edu>
2023-11-17 12:23:41 -0800

    thermocline function, with help added

--- thermocline.m
+++ thermocline.m
@@ -1,32 +1,58 @@
-close all; clear all; clc;
+function [ temp, depth ] = thermocline( TMIN, TMAX, DEPTH, CENTER_OF_GRADIENT, THICKNESS, NPOINTS )
+%% thermocline function:
+% Generates a temperature gradient that mimicks a thermocline.
+%
+% INPUT : 
+%           TMIN : minimum temperature
+%           TMAX : maximum temperature of the gradient
+%           DEPTH : maximum depth of the output ( +meters )
+%           CENTER_OF_GRADIENT : center of transition region ( +meters )
+%           
+%           optional-->
+%           THICKNESS : Thickness of the transition region, 0.1 - 0.001,
+%                       default=0.01, play with this.
+%           NPOINTS : Number of points of output desired, default=1000;
+%
+% OUTPUT :
+%           temp : temperatures (x-axis)
+%           depth : depth, negative values, from 0 to -DEPTH;
+%
+% EXAMPLE :
+%           To generate a thermocline between 10 and 25 degrees (c), with
+%           the center of the transition at -200m, with approximately a
+%           transition width of 100 meters, a maximum depth of 1000
+%           meters, and using 3000 points, the command would be:
+%
+%           [temp, depth] = thermocline(10,25,1000,200,0.001,3000);
+%
+%%
+if (nargin == 5 )
+    NPOINTS = 1000;
+elseif ( nargin == 4 )
+    NPOINTS = 1000;
+    THICKNESS = 0.01;
+end
 
-
-
-TMIN = 3;
-TMAX = 20;
-DEPTH = 1000;
-depth_scale = DEPTH;
-CENTER_OF_GRADIENT = 200;   % Where do you want the switch to take place.
-THICKNESS_OF_TRANSITION = 0.002;  % Thickness of the gradient
-
-
-
-xgrid = linspace(0,1,1000);
+% Settign up the variables for the erf
+depth = linspace(0,1,NPOINTS);
 mu =  (DEPTH-CENTER_OF_GRADIENT)/DEPTH ;
-sigma2 = THICKNESS_OF_TRANSITION;
+sigma2 = THICKNESS;
 denom = sqrt(2*sigma2);
 
-ygrid = TMIN + ((TMAX-TMIN)/2)*(1 + erf( (xgrid - mu)/denom));
-xgrid = xgrid - 1;
-xgrid = xgrid * depth_scale;
+% temperature values calculated here
+temp = TMIN + ((TMAX-TMIN)/2)*(1 + erf( (depth - mu)/denom));
 
-plot(xgrid,ygrid);
+% rescale depth from 0->1 to 0->-depth
+depth = depth - 1;
+depth = depth * DEPTH;
 
-%% Actual Thermocline Values
+%% Plot final distribution
 figure(3)
-plot(ygrid,xgrid,'b','LineWidth',4); grid on;
-xlim([YMIN-5 TMAX + 5]);
+plot(temp,depth,'b','LineWidth',4); grid on;
+xlim([TMIN-5 TMAX + 5]);
 ylim([-DEPTH 0+5]);
 xlabel('Temperature [c]','Interpreter','latex');
 ylabel('Depth [m]','Interpreter','latex');
 title('Thermocline Example Plot');
+
+end
